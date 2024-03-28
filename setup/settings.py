5a09294c9126cfac3fb23901f94481a1921deb83
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2024-03-28 20:52:13 -0300

    configurando o vercel.

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@&tk$+=5fjjxn*bl*@-h(2))q))c551+7+6hxhwpyzi^q!f41@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
